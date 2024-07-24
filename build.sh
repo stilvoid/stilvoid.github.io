8accf1fbedc6a532580dbb9613c6084ae4f99f2e
Steve Engledow <steve@offend.me.uk>
2024-07-24 22:56:51 +0200

    Run shellcheck on build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for f in wiki/diary/20*; do date="$(basename -s .wiki $f | sed -e 's/-//g')0000"; touch -mt $date $f; done
+for f in wiki/diary/20*; do date="$(basename -s .wiki "$f" | sed -e 's/-//g')0000"; touch -mt "$date" "$f"; done
 
 vim "+VimwikiDiaryIndex 2" "+VimwikiDiaryGenerateLinks" "+VimwikiAll2HTML!" "+VimwikiRss" "+q"
