e8a015b92dddfd745ed1d77a7934c196aae79f6a
Steve Engledow <steve@offend.me.uk>
2024-07-31 02:52:22 +0200

    Tidy up, move assets to S3

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp -a static/* ./docs/
+
 for f in wiki/diary/20*; do date="$(basename -s .wiki "$f" | sed -e 's/-//g')0000"; touch -mt "$date" "$f"; done
 
 vim "+VimwikiDiaryIndex 2" "+VimwikiDiaryGenerateLinks" "+VimwikiAll2HTML!" "+VimwikiRss" "+q"
